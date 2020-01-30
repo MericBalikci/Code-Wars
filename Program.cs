a62ae5bfd668541d437d98d393eb276ed8f0f8a4
Meriç <meric.balikci@gmail.com>
2020-01-30 17:45:40 +0300

    Initial Commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Code_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
